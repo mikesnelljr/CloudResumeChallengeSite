75153a5db54df0ec1ac8463614a625820fb1925e
snellularr <134894839+snellularr@users.noreply.github.com>
2024-05-02 22:19:29 -0400

    another counter test

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,10 @@
 // viewcount	
 const counter = document.querySelector(".counter-number");
-	async function updateCounter() {
-			let response = await fetch("https://pxdfv2lutuqkjmsixke35idlry0pkpce.lambda-url.us-east-1.on.aws/");
-			let data = await response.json();
-			counter.innerHTML = ' Views: ${data}';
+async function updateCounter() {
+    let response = await fetch(
+        "https://pxdfv2lutuqkjmsixke35idlry0pkpce.lambda-url.us-east-1.on.aws/"
+    );
+    let data = await response.json();
+    counter.innerHTML = `Views: ${data}`;
 }
-
 updateCounter();
