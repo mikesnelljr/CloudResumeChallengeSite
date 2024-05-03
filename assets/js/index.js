edebac1a55825006649aa7d8263a7f53c82e2d9a
snellularr <134894839+snellularr@users.noreply.github.com>
2024-05-02 22:01:11 -0400

    counter test 4

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,7 @@ const counter = document.querySelector(".counter-number");
 	async function updateCounter() {
 			let response = await fetch("https://pxdfv2lutuqkjmsixke35idlry0pkpce.lambda-url.us-east-1.on.aws/");
 			let data = await response.json();
-			counter.innerHTML = 'views: ${data}';
+			counter.innerHTML = ' Views: ${data}';
 }
 
 updateCounter();
